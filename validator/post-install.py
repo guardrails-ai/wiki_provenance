99c6cd101824068551257fc6a8cea318d9b6c509
Karan Acharya <karan@guardrailsai.com>
2024-03-18 12:31:42 -0400

    Remove utils, move installation to post install

--- validator/post-install.py
+++ validator/post-install.py
@@ -1,9 +1,27 @@
+import subprocess
+import sys
+
 import nltk
 
-from .utils.chroma_utils import install_chroma
+# Install chromadb (and pysqlite3-binary on linux)
+try:
+    if sys.platform in ("linux", "linux2"):
+        # If on linux, install pysqlite3-binary and follow steps as mentioned here:
+        # https://gist.github.com/defulmere/8b9695e415a44271061cc8e272f3c300
+        print("Installing `pysqlite3-binary`...")
+        subprocess.check_call(
+            [sys.executable, "-m", "pip", "install", "pysqlite3-binary"]
+        )
+        __import__("pysqlite3")
+        sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 
-# Install chromadb
-install_chroma()
+    # Install chromadb
+    print("Installing `chromadb`...")
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "chromadb", "-q"])
+    print("Successfully installed `chromadb`.")
+except Exception as e:
+    raise RuntimeError(f"Failed to install `chromadb`:\n{e}") from e
+
 
 # Download NLTK data if not already present
 try:
