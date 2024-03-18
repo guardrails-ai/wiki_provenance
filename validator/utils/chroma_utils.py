070f0cb7d0e8853c00a9a93f213fe632ad083c66
Karan Acharya <karan@guardrailsai.com>
2024-03-18 12:23:52 -0400

    Rename file

--- validator/utils/chroma_utils.py
+++ validator/utils/chroma_utils.py
@@ -1,9 +1,8 @@
-import os
 import subprocess
 import sys
 
 
-def install_chroma() -> bool:
+def install_chroma():
     """Installs chromadb correctly based on the OS.
 
     Returns:
@@ -14,26 +13,17 @@ def install_chroma() -> bool:
         if sys.platform in ("linux", "linux2"):
             # If on linux, install pysqlite3-binary and follow steps as mentioned here:
             # https://gist.github.com/defulmere/8b9695e415a44271061cc8e272f3c300
+            print("Installing `pysqlite3-binary`...")
             subprocess.check_call(
-                [sys.executable, "-m", "pip", "install", "pysqlite3-binary", "-q"]
+                [sys.executable, "-m", "pip", "install", "pysqlite3-binary"]
             )
             __import__("pysqlite3")
-
             sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
-            BASE_DIR = os.getcwd()
 
-            DATABASES = {
-                "default": {
-                    "ENGINE": "django.db.backends.sqlite3",
-                    "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-                }
-            }
-            print(DATABASES)
-
-        # Install requirements
+        # Install chromadb
         subprocess.check_call(
             [sys.executable, "-m", "pip", "install", "chromadb", "-q"]
         )
-        return True
-    except Exception:
-        return False
+        print("Successfully installed `chromadb`.")
+    except Exception as e:
+        raise RuntimeError(f"Failed to install `chromadb`:\n{e}") from e
